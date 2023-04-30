3d6d956b83b3bb0239e4929505343a59c91e7d93
Nattapong Chokchaiworrarat <nattapong.ccwr@gmail.com>
2023-05-01 01:09:09 +0700

    first commit

---
+++ src/components/customMessageForms/AICode.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import MessageFormUl from './messageFormUl';
+import { usePostAiCodeMutation } from '@/state/api';
+
+function AICode({ props, activeChat }) {
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [triggerCode] = usePostAiCodeMutation();
+
+    const handleChange = (e) => setMessage(e.target.value);
+
+    const handleSubmit = async () => {
+        const date = new Date().toISOString().replace("T", " ").replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+        const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+        const form = {
+            attachments: at,
+            created: date,
+            sender_username: props.username,
+            text: message,
+            activeChatId: activeChat.id,
+        };
+
+        props.onSubmit(form);
+        triggerCode(form);
+        setMessage("");
+        setAttachment("");
+    };
+    return (
+        <MessageFormUl
+            setAttachment={setAttachment}
+            message={message}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+        />
+    )
+}
+
+export default AICode
