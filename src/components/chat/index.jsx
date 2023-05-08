d8835d68a06f4fcddd6144b21fc36808b2b40d08
Nattapong Chokchaiworrarat <nattapong.ccwr@gmail.com>
2023-05-08 11:10:23 +0700

    update auth

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -8,9 +8,10 @@ import Header from "@/components/customHeader"
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 import AI from '@/components/customMessageForms/AI'
 import AICode from '@/components/customMessageForms/AICode'
+import AIAssist from '@/components/customMessageForms/AIAssist'
 
 
-const Chat = () => {
+const Chat = ({ user, secret }) => {
     const chatProps = useMultiChatLogic(
         import.meta.env.VITE_PROJECT_ID,
         "testuser",
@@ -30,6 +31,9 @@ const Chat = () => {
                 }
                 if (chatProps.chat?.title.startsWith("AiCode_")) {
                     return <AICode props={props} activeChat={chatProps.chat} />
+                }
+                if (chatProps.chat?.title.startsWith("AiAssist_")) {
+                    return <AIAssist props={props} activeChat={chatProps.chat} />
                 }
 
                 return ( 
